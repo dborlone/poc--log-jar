181c80ee589be7694bf9b59a9a3854b8d3b3b186
dromualdo <48302570+dborlone@users.noreply.github.com>
2021-06-22 04:17:22 -0300

    Versao inicial
    Testes com logs

---
+++ src/main/java/br/com/borlone/poc/log/teste/Main.java
@@ -0,0 +1,14 @@
+package br.com.borlone.poc.log.teste;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("O log funcionou!");
+        LOGGER.error("Deu ruim... :-/ .. Na verdade deu BOM :-)");
+    }
+}
